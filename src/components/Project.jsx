2a356da40c036a375011c0edccb96190e6940fc0
Noor Mohammad <noormohammad.net1503011@gmail.com>
2022-12-25 11:50:17 +0600

    changing image width

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,11 @@ const Project = ({ item }) => {
   return (
     <div key={item.id} className='flex flex-col items-center text-center'>
       <div className='mb-8'>
-        <img className='rounded-2xl w-[350px] h-[300px]' src={item.image} alt={item.image} />
+        <img
+          className='rounded-2xl w-[350px] h-[300px]'
+          src={item.image}
+          alt={item.image}
+        />
       </div>
       <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
       <h3 className='text-2xl font-semibold capitalize mb-3'>{item.name}</h3>
